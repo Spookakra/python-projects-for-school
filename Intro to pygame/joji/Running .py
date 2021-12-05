157c567ec50f5c15d178a4f9344057dc02fe27d7
Spookakra <62347712+Spookakra@users.noreply.github.com>
2021-12-05 15:19:03 -0500

    Create Running .py

---
+++ Intro to pygame/joji/Running .py
@@ -0,0 +1,18 @@
+#endless runner gamming will be here 
+import pygame 
+
+#important (put at top) it like does the whole pygame thing
+pygame.init()
+
+#create a blank window (width, height)
+screen = pygame.display.set_mode((800, 400))
+
+
+
+
+
+
+
+
+
+#the product of learning from the gammeing man: https://www.youtube.com/watch?v=AY9MnQ4x3zk
