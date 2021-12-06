698cd356f9e2a5f38d5094c6dda21b7a916e5374
Spookakra <62347712+Spookakra@users.noreply.github.com>
2021-12-06 09:09:45 -0500

    tim did this

--- Mini Projects/Password manager/password_manager.py
+++ Mini Projects/Password manager/password_manager.py
@@ -1,13 +1,13 @@
 #lasspass2
 from cryptography.fernet import Fernet
 
-'''
 #create a key file named "key.key"
+'''
 def write_key():
     key = Fernet.generate_key()
-    with open("Key.key", "wb") as key_file:
+    with open("key.key", "wb") as key_file:
         key_file.write(key)'''
-    
+
 #loads the key file
 def load_key():
     file = open("key.key", "rb")
@@ -16,49 +16,47 @@ def load_key():
     return key
 
 
-
-master_pwd = input("What is the master password?")
 #encryption 😏
-
-key = load_key() + master_pwd.encode()
+key = load_key()
 fer = Fernet(key)
 
 
 
 #view existing passwords
 def view():
-    with open('passwords.txt', 'r') as f:  
+    with open('passwords.txt', 'r') as f:
         for line in f.readlines():
-            data = (line.rstrip())
-            user , passw = data.split("|")
-            print("User:", user, "| Password:", 
-            str(fer.decrypt(passw.encode())))
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
 
-    
+
 
 #add new passwords
 def add():
-    name = input('account name: ')
+    name = input('Account Name: ')
     pwd = input("Password: ")
 
-    with open('passwords.txt', 'a') as f:  
-        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 
-while True:
-    mode = input("Would you like to add a new password or view existing ones(view, add)?, Press q to quit").lower()
-    if mode == "q":
-        break
 #ifthen
 #select the mode
+while True:
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
     if mode == "view":
         view()
     elif mode == "add":
-        add()  
-
-    else: 
-        print("Invalid Mode.")
+        add()
+    else:
+        print("Invalid mode.")
         continue
 
 #python file opening modes ig 
