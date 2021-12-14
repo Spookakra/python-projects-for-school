02cd53b77c505a1012f2b162dd7c74a7d3547a92
Spookakra <62347712+Spookakra@users.noreply.github.com>
2021-12-14 10:11:13 -0500

    Create pound it.py

---
+++ work in progress/desktop ui test/pound it.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import webbrowser
+
+link = "deathgenerator.com"
+
+window = tk.Tk()
+window.title("death window")
+window.geometry("600x500")
+
+#result of button press
+def grindsetevent(event):
+    print("i am in a lot of pain")
+    webbrowser.open(link)
+
+#simple 1999 button
+grindsetman = tk.Button(window, text = "click me")
+grindsetman.grid(column=8, row=5)
+grindsetman.bind("<Button-1>",grindsetevent)
+
+
+
+
+
+
+
+window.mainloop()
