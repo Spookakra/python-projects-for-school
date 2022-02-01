fa35f20a677e0ab4c5e3f303bfb580595df091bd
Spookakra <62347712+Spookakra@users.noreply.github.com>
2022-02-01 09:38:57 -0500

    gameing updated

---
+++ homework/119.py
@@ -0,0 +1,26 @@
+#animal generator (maybe)
+import turtle as bug
+
+input("choose a creature.")
+
+while True:
+    grievances = input("choose a number. 1-3")
+    if grievances == "1":
+        print("commence the dice roll")
+        print("you rolled a", first_dice_roll)
+        break
+    elif grievances == "n":
+        print("ok then")
+        quit()
+
+
+    else:
+        print("that's not an answer")
+        continue
+
+
+wn = bug.Screen()
+wn.mainloop()
+
+
+#i am stuck in a perpetual loop of pain
