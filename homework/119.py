b7b5fcb3f9608501ab3527adff5f588ddfdfb04a
Spookakra <62347712+Spookakra@users.noreply.github.com>
2022-02-03 10:18:01 -0500

    Update 119.py

--- homework/119.py
+++ homework/119.py
@@ -1,26 +1,92 @@
-#animal generator (maybe)
+#animal generator 
 import turtle as bug
+import time 
 
-input("choose a creature.")
 
 while True:
-    grievances = input("choose a number. 1-3")
+    print("choose a creature")
+    grievances = input("1: Ladybug, 2: Spider, 3: Prog god, q: quit ")
     if grievances == "1":
-        print("commence the dice roll")
-        print("you rolled a", first_dice_roll)
+
+        # create ladybug head
+        ladybug = bug.Turtle()
+        ladybug.pensize(40)
+        ladybug.circle(5)
+        ladybug.speed(0)
+
+        # and body
+        ladybug.penup()
+        ladybug.goto(0, -55) 
+        ladybug.color("red")
+        ladybug.pendown()
+        ladybug.pensize(40)
+        ladybug.circle(20)
+        ladybug.setheading(270)
+        ladybug.color("black")
+        ladybug.penup()
+        ladybug.goto(0, 5)
+        ladybug.pensize(2)
+        ladybug.pendown()
+        ladybug.forward(75)
+
+        # config dots
+        xpos = -20
+        ypos = -55
+        ladybug.pensize(10)
+
+        # draw two sets of dots
+
+        ladybug.penup()
+        ladybug.goto(xpos, ypos)
+        ladybug.pendown()
+        ladybug.circle(4)
+        ladybug.penup()
+        ladybug.goto(xpos + 30, ypos + 20)
+        ladybug.pendown()
+        ladybug.circle(4)
+
+        # position next dots
+        ypos = ypos + 35
+        xpos = xpos + 3
+        ladybug.penup()
+
+        #draw more dots
+        ladybug.goto(xpos, ypos)
+        ladybug.hideturtle()
+        ladybug.pendown()
+        ladybug.circle(4)
+
+        # position more dots
+        ypos = ypos + 16
+        xpos = xpos + 25
+        ladybug.penup() 
+
+        #DOTS 
+        ladybug.goto(xpos, ypos)
+        ladybug.hideturtle()
+        ladybug.pendown()
+        ladybug.circle(4)
+
+        time.sleep(3)
+        bug.clearscreen()
+        continue
+
+
+
+    elif grievances == "2":
+        pass
+
+    elif grievances == "3":
+        pass
+
+
+    elif grievances == "q":
         break
-    elif grievances == "n":
-        print("ok then")
-        quit()
 
 
     else:
         print("that's not an answer")
         continue
-
-
-wn = bug.Screen()
-wn.mainloop()
 
 
 #i am stuck in a perpetual loop of pain
